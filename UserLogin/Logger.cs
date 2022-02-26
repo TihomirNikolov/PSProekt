dd9e26c007494f26ae7af38f169e689aa0a305c4
TihomirNikolov <tihomirnikolov2@gmail.com>
2022-02-26 18:13:41 +0200

    Add project files.

---
+++ UserLogin/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLogin
+{
+    public static class Logger
+    {
+        private static List<string> currentSessionActivities = new List<string>();
+
+        public static void LogActivity(string activity)
+        {
+            string activityLine = "************************************************\n" +
+                DateTime.Now + "\n"
+                + LoginValidation.CurrentUsername + "\n"
+                + LoginValidation.CurrentUserRole + "\n"
+                + activity + "\n************************************************\n";
+            currentSessionActivities.Add(activityLine);
+
+            File.AppendAllText("LogFile.txt", activityLine);
+            File.Delete(Path.GetFullPath(@"..\..\..\LogFile.txt"));
+            File.Copy("LogFile.txt", Path.GetFullPath(@"..\..\..\LogFile.txt"));
+        }
+
+        public static string GetCurrentSessionActivities()
+        {
+            StringBuilder sessionActivities = new StringBuilder();
+
+            foreach (var currentSession in currentSessionActivities)
+            {
+                sessionActivities.Append(currentSession);
+            }
+
+            return sessionActivities.ToString();
+        }
+    }
+}
---
+++ UserLogin/UserData.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLogin
+{
+    public static class UserData
+    {
+        private static List<User> testUsers;
+
+        public static List<User> TestUsers
+        {
+            get
+            {
+                ResetTestUserData();
+                return testUsers;
+            }
+            set { }
+        }
+
+        static public void ResetTestUserData()
+        {
+            testUsers = new List<User>();
+
+            testUsers.Add(new User()
+            {
+                Username = "admin",
+                Password = "admin",
+                UserRole = UserRoles.ADMIN,
+                FacultyNumber = "111111111",
+                DateOfCreation = DateTime.Now,
+                DateActiveTo = DateTime.MaxValue
+            });
+
+            testUsers.Add(new User()
+            {
+                Username = "student1",
+                Password = "student",
+                UserRole = UserRoles.STUDENT,
+                FacultyNumber = "123456789",
+                DateOfCreation = DateTime.Now,
+                DateActiveTo = DateTime.MaxValue
+            });
+
+            testUsers.Add(new User()
+            {
+                Username = "student2",
+                Password = "student",
+                UserRole = UserRoles.STUDENT,
+                FacultyNumber = "123456790",
+                DateOfCreation = DateTime.Now,
+                DateActiveTo = DateTime.MaxValue
+            });
+        }
+
+        public static User IsUserPassCorrect(string username, string password)
+        { 
+            return TestUsers.Where(u => u.Username == username && u.Password == password).FirstOrDefault();
+
+            /*foreach(var user in TestUsers)
+            {
+                if(user.Username == username && user.Password == password)
+                {
+                    return user;
+                }
+            }
+            return null;*/
+        }
+
+        public static void SetUserActiveTo(string username, DateTime dateActiveTo)
+        {
+            foreach(var user in TestUsers)
+            {
+                if(user.Username == username)
+                {
+                    user.DateActiveTo = dateActiveTo;
+                    Console.Write("Датата променена на " + dateActiveTo.ToShortDateString());
+                    Logger.LogActivity("Промяна на роля на " + username);
+                }
+            }
+        }
+
+        public static void AssignUserRole(string username, UserRoles userRole)
+        {
+            foreach (var user in TestUsers)
+            {
+                if (user.Username == username)
+                {
+                    user.UserRole = userRole;
+                    Console.Write("Ролята променена на " + userRole);
+                    Logger.LogActivity("Промяна на активност на " + username);
+                }
+            }
+        }
+    }
+}
